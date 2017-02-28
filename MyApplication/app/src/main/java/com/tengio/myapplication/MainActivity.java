5ef59a13d1eb78702c1710d5212ce793ee17a414
Tengio Ltd <tengio@Tengios-MacBook-Pro.local>
2017-02-28 16:37:35 +0000

    Various changes

--- MyApplication/app/src/main/java/com/tengio/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/tengio/myapplication/MainActivity.java
@@ -7,6 +7,7 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.hardware.camera2.CameraManager;
 import android.media.ExifInterface;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.media.MediaMetadataCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -21,13 +22,14 @@ import android.widget.Toast;
 
 import com.googlecode.tesseract.android.TessBaseAPI;
 
+import java.io.File;
 import java.io.IOException;
 
 import static android.R.attr.bitmap;
 import static android.R.attr.mimeType;
 
 public class MainActivity extends AppCompatActivity {
-    public final static int REQUEST_IMAGE_CAPTURE = 0;
+    public final static int REQUEST_IMAGE_CAPTURE = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,32 +46,40 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
-            Bundle extras = data.getExtras();
-            //Bitmap imageBitmap = (Bitmap) extras.get("data");
-            Bitmap imageBitmap = null;
-            try {
-                imageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), data.getData());
-            } catch (IOException e) {
-                e.printStackTrace();
+        if (resultCode != RESULT_CANCELED)
+            if (requestCode == REQUEST_IMAGE_CAPTURE) {
+                Bundle extras = data.getExtras();
+                //Bitmap imageBitmap = (Bitmap) extras.get("data");
+                Bitmap imageBitmap = null;
+
+                try {
+                    imageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), data.getData());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                if (imageBitmap != null) {
+                    Log.w("Width", String.valueOf(imageBitmap.getWidth()));
+
+                    Log.w("Height", String.valueOf(imageBitmap.getHeight()));
+                    Log.w("Loc", getFilesDir().getAbsolutePath());
+
+                    ImageView mImageView = (ImageView) findViewById(R.id.imageView);
+
+                    Drawable bd = new BitmapDrawable(getResources(), imageBitmap);
+                    mImageView.setImageDrawable(bd);
+                    mImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+                    TessBaseAPI tessBaseAPI = new TessBaseAPI();
+                    File tessSubDir = new File(Environment.getDataDirectory(), "/tessdata");
+
+                    tessSubDir.mkdirs();
+
+                    tessBaseAPI.init(Environment.getDataDirectory().getAbsolutePath(), "eng");
+                    tessBaseAPI.setImage(imageBitmap);
+
+                    Toast.makeText(MainActivity.this, "Toast: " + tessBaseAPI.getUTF8Text(), Toast.LENGTH_LONG).show();
+                }
             }
-
-            Log.w("Width", String.valueOf(imageBitmap.getWidth()));
-            Log.w("Height", String.valueOf(imageBitmap.getHeight()));
-            Log.w("Loc", getFilesDir().getAbsolutePath());
-
-            ImageView mImageView = (ImageView) findViewById(R.id.imageView);
-
-            Drawable bd = new BitmapDrawable(getResources(), imageBitmap);
-            mImageView.setImageDrawable(bd);
-            mImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-
-            TessBaseAPI tessBaseAPI = new TessBaseAPI();
-
-            tessBaseAPI.setImage(imageBitmap);
-            tessBaseAPI.init(getFilesDir() + "/tessdata/", "eng");
-
-            Toast.makeText(MainActivity.this, "Toast: " + tessBaseAPI.getUTF8Text(), Toast.LENGTH_LONG).show();
-        }
     }
 }
